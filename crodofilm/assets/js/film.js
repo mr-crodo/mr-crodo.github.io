3759f9e1d802d69e8c1499c80666736d5538ed0a
Nasib Mammadov <bduti09@gmail.com>
2020-06-30 01:09:54 +0400

    foter finish

---
+++ crodofilm/assets/js/film.js
@@ -0,0 +1,264 @@
+
+
+// **************** etot kod menaet svg zalivku v img elemente
+
+$('img.footer-batman').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+
+$('img.footer-game').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.footer-cat').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+
+
+// ******************* footer ico 
+
+$('img.email').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.facebook').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.github-kote').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.instagram').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.linkedin').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.odnoklassniki').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.pinterest').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.telegram').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.twitter').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.vk').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.whatsapp').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
+$('img.wordpress').each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function (data) {
+    var $svg = $(data).find('svg');
+    if (typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass + ' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+
