8da492d82015bc727d0f971d689c9d75a796df03
Nasib Mammadov <bduti09@gmail.com>
2020-07-04 02:42:39 +0400

    04.07 finish

--- aviasales/assets/js/main.js
+++ aviasales/assets/js/main.js
@@ -1,9 +1,157 @@
+// $(function () {
+//   var $zip = $('[name="zip"]'),
+//       $region = $('[name="region"]'),
+//       $district = $('[name="district"]'),
+//       $city = $('[name="city"]'),
+//       $street = $('[name="street"]'),
+//       $building = $('[name="building"]');
+
+//   var $tooltip = $('.tooltip');
+
+//   $.fias.setDefault({
+//       parentInput: '.js-form-address',
+//       verify: true,
+//       select: function (obj) {
+//           setLabel($(this), obj.type);
+//           $tooltip.hide();
+//       },
+//       check: function (obj) {
+//           var $input = $(this);
+
+//           if (obj) {
+//               setLabel($input, obj.type);
+//               $tooltip.hide();
+//           }
+//           else {
+//               showError($input, 'Введено неверно');
+//           }
+//       },
+//       checkBefore: function () {
+//           var $input = $(this);
+
+//           if (!$.trim($input.val())) {
+//               $tooltip.hide();
+//               return false;
+//           }
+//       },
+//       change: function (obj) {
+//           if(obj && obj.parents){
+//               $.fias.setValues(obj.parents, '.js-form-address');
+//           }
+
+//           if(obj && obj.zip){
+//               $('[name="zip"]').val(obj.zip);
+//           }
+//       },
+//   });
+
+//   $region.fias('type', $.fias.type.region);
+//   $district.fias('type', $.fias.type.district);
+//   $city.fias('type', $.fias.type.city);
+//   $street.fias('type', $.fias.type.street);
+//   $building.fias('type', $.fias.type.building);
+
+//   $district.fias('withParents', true);
+//   $city.fias('withParents', true);
+//   $street.fias('withParents', true);
+
+
+//   // Отключаем проверку введённых данных для строений
+//   $building.fias('verify', false);
+
+//   // Подключаем плагин для почтового индекса
+//   // $zip.fiasZip('.js-form-address');
+
+//   function setLabel($input, text) {
+//       text = text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
+//       $input.parent().find('label').text(text);
+//   }
+
+//   function showError($input, message) {
+//       $tooltip.find('span').text(message);
+
+//       var inputOffset = $input.offset(),
+//           inputWidth = $input.outerWidth(),
+//           inputHeight = $input.outerHeight();
+
+//       var tooltipHeight = $tooltip.outerHeight();
+
+//       $tooltip.css({
+//           left: (inputOffset.left + inputWidth + 10) + 'px',
+//           top: (inputOffset.top + (inputHeight - tooltipHeight) / 2 - 1) + 'px'
+//       });
+
+//       $tooltip.show();
+//   }
+// });
+
+
+
+
+
+
+// $(function () {
+
+
+  
+//   $('[name="city"]').fias({
+//   type: $.fias.type.city,
+//       change: function (obj) {
+//           var address = $.fias.getAddress('.js-form-address');
+
+//           $('#address').text(address);
+//       },
+//       'withParents' : true
+
+//   });
+
+
+// });
+
+// $(function () {
+//   var $city = $('[name="city"]');
+
+//   $city.fias({
+//       type: $.fias.type.city,
+//       'withParents': true,
+//       change: function (obj) {
+//           var address = $.fias.getAddress('.js-form-address');
+
+//           $('#address').text(address);
+//       },
+//   });
+
+//   $('[name="city_id"]').change(function () {
+//       var id = $(this).val();
+
+//       // Устанавливаем значение поля ввода по id
+//       $city.fias('controller').setValueById(id);
+//   });
+// });
+
+
+
+// $.fias.token = 'KfkdT6z4FnBynBKknQ8t6Kr7b5e2BHei';
+//     $.fias.url = 'https://kladr-api.ru/api.php';
+
+
+
+
+
+
+
+
 const formSearch = document.querySelector('.form-search'),
-  inputCitiesFrom = document.querySelector.apply('.input__cities-from'),
+  inputCitiesFrom = document.querySelector('.input__cities-from'),
   dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
   inputCitiesTo = document.querySelector('.input__cities-to'),
   dropdownCitiesTo = document.querySelector('dropdown_cities-to'),
   inputDateDepart = document.querySelector('.input__date-depart');
+
+
+//  eto moy token ot sayta  https://kladr-api.ru/profile, dla toqo
+// ctobi ne ispolzovat bolshoe kolicestvo massivov dla stran
+// KfkdT6z4FnBynBKknQ8t6Kr7b5e2BHei
 
 
 const city = [
@@ -334,3 +482,15 @@ const city = [
 inputCitiesFrom.addEventListener('input', () => {
 console.log("sobitie input");
 });
+
+// function show() {
+//   console.log('Vizov shov');
+// }
+
+// show();
+
+const get = () => {
+  console.log('Vizov get');
+};
+
+get();
