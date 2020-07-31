b443a47f8b693c955c5eff69d5d01eeec435d278
Nasib Mammadov <bduti09@gmail.com>
2020-07-31 23:15:35 +0400

    volume

--- itunes/assets/js/videoPlayer.js
+++ itunes/assets/js/videoPlayer.js
@@ -27,6 +27,14 @@ export const videoPlayerInit = () => {
   const videoTimePassed = document.querySelector('.video-time__passed');
   const videoTimeTotal = document.querySelector('.video-time__total');
 
+  // syda vstavlaem konstantu dla toqo ctobi mojno bilo pereklycat zvuk video
+  const videoVolume = document.querySelector('.video-volume');
+
+  // ? sdes stavim konstantu dla toqo ctobi sdelat video na ves ekran ili full screen
+  const videoFullscreen = document.querySelector('.video-fullscreen');
+
+  
+
 
   // TODO: mi sdes zapisivaem otdelnuyu fukciyu ctob moqli obrahsatsa k klassu favicon play
   // TODO: ctob moqli pomenat eqo na faicon pause
@@ -71,111 +79,137 @@ export const videoPlayerInit = () => {
 
   // ! verxnuyu funkciyu mi zakomentirovali ctob neskolko raz ne pisat odno i toje
 
-// ! dla toqo ctobi mi ne pisali odnu i tu je funkciyu neskolko raz
-// ! mi zapisali dla neqo konstantu
-const togglePlay = () => {
-  if (videoPlayer.paused) {
-    videoPlayer.play();
-  } else {
+  // ! dla toqo ctobi mi ne pisali odnu i tu je funkciyu neskolko raz
+  // ! mi zapisali dla neqo konstantu
+  const togglePlay = () => {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    }
+
+    // !vmesto toggle eto obrashenie k ikonkam pause i play
+    // ? ctobi mojno bilo izmenit iconki
+    // TODO: eqo mojno i napisat po druqomu a kak ya zapisal vnizu i pometil flaqom
+    toggleIcon();
+  };
+
+
+  // TODO: sdes zapisivaem funkciyu stopPlay dla ostanovki video
+  // TODO: po najatiyu na stop ikonki
+  const stopPlay = () => {
+    // ? sdes mi stavim video na pauzu
     videoPlayer.pause();
+    // ? a sdes vozvrashaem vrema v nulevuyu poziciyu koqda najimaetsa na pauzu
+    videoPlayer.currentTime = 0;
+
+    // ? sdes mi zapisivaem toggle dla izmeneniya ikonki
+    toggleIcon();
   }
+  // ! sdes mi sozdaem konstantu ctob v nacalo video dobavit nuli radom so vremenem
+  // ? sdes mi zapisali ternarniy operator
+  // !             => условие ? (условие верно) : ( условие лож);
+  const addZero = n => n < 10 ? '0' + n : n;
+  //Todo  vercneye mojno zapisat i tak
+  // const addZero = n => {
+  //   return n < 10 ? '0' + n : n;
+  // }
 
-  // !vmesto toggle eto obrashenie k ikonkam pause i play
-  // ? ctobi mojno bilo izmenit iconki
-  // TODO: eqo mojno i napisat po druqomu a kak ya zapisal vnizu i pometil flaqom
-  toggleIcon();
+
+
+
+  // ! na samom dele vmesto togglePlay doljna bila bit  verxnaya funkciya
+  videoPlayer.addEventListener('click', togglePlay);
+  // ! sdes mi stavim slushital ctob poymat click po klassu a klass
+  // ! mi vstavili pod konstantu kotoraya nazivaetsa video button play
+  // ! class video button play etot to klass tot element kotoriy 
+  // ! stoit vnizu video pod kartinkoy play menayusheysa v pause
+  // ! i posle k ney propisivaem funkciyu togglePlay
+  videoButtonPlay.addEventListener('click', togglePlay);
+
+  // ?$$$$$$$$$$$$$$$
+  // ! $$$$$$$$$$$$$$
+  // *$$$$$$$$$$$$$$$ poka ya ix zakomentiroval tak kak v verxu toggle ostalsa
+  // videoPlayer.addEventListener('play', toggleIcon);
+  // videoPlayer.addEventListener('pause', toggleIcon);
+
+  // TODO: sdes mi delaem pauzu po knobke
+  // ?dla etoqoo stavim slushatel i otlavlivaem klas pauzu knobki v html
+  // ? klass kotoroqo mi otmetili constantoy i zapisali sdes v nacale
+  videoButtonStop.addEventListener('click', stopPlay);
+
+  // TODO: sdes mi stavim esho odin obrobotcik sobitiy dla toqo ctobi izmenat vrema
+  videoPlayer.addEventListener('timeupdate', () => {
+    // eto to vrema kotoroe prosho
+    const currentTime = videoPlayer.currentTime;
+    // a eto vrema vseqo video ono ne izmenaetsa
+    const duration = videoPlayer.duration;
+    // ? console eto proverka kak v CSS border
+    // console.log(currentTime);
+    // console.log(duration);
+
+    // TODO: dla toqo ctobi rabotal polzunok vstavlaem progress 
+    // ? sdes mi delelm proydennoe vreme na ostavsheyesa ctob ponat skolko ostalos vremeni iz vseqo vremeni
+    videoProgress.value = (currentTime / duration) * 100;
+
+    // TODO: sdes mi propisivaem peremennuyu ctobi pri proxojdenii vremeni 60s pokazivalo 1 minutu
+    let minutePassed = Math.floor(currentTime / 60);
+    // TODO: a sdes mi polucaem ostatok ot deleniya, eto i budet sekundi
+    let secondsPassed = Math.floor(currentTime % 60);
+
+    /*
+    CT - 35 мин / sek
+    duration 100min / sek
+
+    15 sek / 100 * 100 = 15 sekunda
+    35/ 100 * 100 = 35
+
+
+    */
+
+    let minuteTotal = Math.floor(duration / 60);
+    let secondsTotal = Math.floor(duration % 60);
+    // sdes toje stavim 
+    // console.log(secondsPassed, secondsTotal);
+
+    // ! teper nam nado vivesti eti dannie sekundi i menuti na ekran
+    // ! a tocnee radom s video
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`
+    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`
+    // videoTimeTotal.textContent = addZero(minuteTotal) + ':' + addZero(secondsTotal);
+
+    // videoTimePassed.textContent = addZero(minutePassed) + ':' + addZero(secondsPassed);
+    // !verxniy element v komente mojem zapisat kak vnizu shablonnoy strockoy
+    // ? stavim obratnie kovicki ot edenici s levo
+    // videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`
+
+  });
+
+  // ! sdes mi dobavlaem fukciyu ctob mojno bilo izmenat video
+  videoProgress.addEventListener('change', () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+
+    // ? sdes mi stavim mestopolojenie ikonki po shkale video(string), v zavismosti ot eqo vremennoy ampletudi
+    videoPlayer.currentTime = (value * duration) / 100;
+  });
+
+// * FULLSCREEN
+  //! sdes vipolnaem otkrit na ves ekran po kliku
+  videoFullscreen.addEventListener('click', () => {
+    videoPlayer.webkitEnterFullScreen();
+    // nam daje ne prishlos delat obratnoe viplnenie zakritie fullscren po kliku
+  });
+
+  // * ZVUK
+  // ! sdes s pomosyu obrabotcika sobitiya izmenaem uroven zvuka
+  videoVolume.addEventListener('input', () => {
+    videoPlayer.volume = videoVolume.value / 100;
+  });
+
+  // todo: sdes mi vstavlaem pervonacalnoe polojenie zvuka 0.5 eto 50%
+  videoPlayer.volume = 0.5;
+
+  videoVolume.value = videoPlayer.volume * 100;
+
 };
-
-
-// TODO: sdes zapisivaem funkciyu stopPlay dla ostanovki video
-// TODO: po najatiyu na stop ikonki
-const stopPlay = () => {
-  // ? sdes mi stavim video na pauzu
-  videoPlayer.pause();
-  // ? a sdes vozvrashaem vrema v nulevuyu poziciyu koqda najimaetsa na pauzu
-  videoPlayer.currentTime = 0;
-
-  // ? sdes mi zapisivaem toggle dla izmeneniya ikonki
-  toggleIcon();
-}
-// ! sdes mi sozdaem konstantu ctob v nacalo video dobavit nuli radom so vremenem
-// ? sdes mi zapisali ternarniy operator
-// !             => условие ? (условие верно) : ( условие лож);
-const addZero = n => n < 10 ? '0' + n : n;
-//Todo  vercneye mojno zapisat i tak
-// const addZero = n => {
-//   return n < 10 ? '0' + n : n;
-// }
-
-
-
-
-// ! na samom dele vmesto togglePlay doljna bila bit  verxnaya funkciya
-videoPlayer.addEventListener('click', togglePlay );
-// ! sdes mi stavim slushital ctob poymat click po klassu a klass
-// ! mi vstavili pod konstantu kotoraya nazivaetsa video button play
-// ! class video button play etot to klass tot element kotoriy 
-// ! stoit vnizu video pod kartinkoy play menayusheysa v pause
-// ! i posle k ney propisivaem funkciyu togglePlay
-videoButtonPlay.addEventListener('click', togglePlay);
-
-// ?$$$$$$$$$$$$$$$
-// ! $$$$$$$$$$$$$$
-// *$$$$$$$$$$$$$$$ poka ya ix zakomentiroval tak kak v verxu toggle ostalsa
-// videoPlayer.addEventListener('play', toggleIcon);
-// videoPlayer.addEventListener('pause', toggleIcon);
-
-// TODO: sdes mi delaem pauzu po knobke
-// ?dla etoqoo stavim slushatel i otlavlivaem klas pauzu knobki v html
-// ? klass kotoroqo mi otmetili constantoy i zapisali sdes v nacale
-videoButtonStop.addEventListener('click', stopPlay);
-
-// TODO: sdes mi stavim esho odin obrobotcik sobitiy dla toqo ctobi izmenat vrema
-videoPlayer.addEventListener('timeupdate', () => {
-  // eto to vrema kotoroe prosho
-  const currentTime = videoPlayer.currentTime;
-  // a eto vrema vseqo video ono ne izmenaetsa
-  const duration = videoPlayer.duration;
-  // ? console eto proverka kak v CSS border
-  // console.log(currentTime);
-  // console.log(duration);
-
-  // TODO: dla toqo ctobi rabotal polzunok vstavlaem progress 
-  // ? sdes mi delelm proydennoe vreme na ostavsheyesa ctob ponat skolko ostalos vremeni iz vseqo vremeni
-  videoProgress.value = (currentTime / duration) * 100;
-
-  // TODO: sdes mi propisivaem peremennuyu ctobi pri proxojdenii vremeni 60s pokazivalo 1 minutu
-  let minutePassed = Math.floor(currentTime / 60);
-  // TODO: a sdes mi polucaem ostatok ot deleniya, eto i budet sekundi
-  let secondsPassed = Math.floor(currentTime % 60);
-
-  let minuteTotal = Math.floor(duration / 60);
-  let secondsTotal = Math.floor(duration % 60);
-// sdes toje stavim 
-  // console.log(secondsPassed, secondsTotal);
-
-  // ! teper nam nado vivesti eti dannie sekundi i menuti na ekran
-  // ! a tocnee radom s video
-  videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`
-  videoTimePassed.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`
-  // videoTimeTotal.textContent = addZero(minuteTotal) + ':' + addZero(secondsTotal);
-
-  // videoTimePassed.textContent = addZero(minutePassed) + ':' + addZero(secondsPassed);
-  // !verxniy element v komente mojem zapisat kak vnizu shablonnoy strockoy
-  // ? stavim obratnie kovicki ot edenici s levo
-  // videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`
-  
-});
-
-// ! sdes mi dobavlaem fukciyu ctob mojno bilo izmenat video
-videoProgress.addEventListener('change', () => {
-  const duration = videoPlayer.duration;
-  const value = videoProgress.value;
-
-  // ? sdes mi stavim mestopolojenie ikonki, v zavismosti ot eqo vremennoy ampletudi
-  videoPlayer.currentTime = (value * duration) / 100;
-})
-
-
-};
-
