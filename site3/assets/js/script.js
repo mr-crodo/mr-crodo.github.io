b9900333d1c1dfa1a631220c1aabed6047f58470
Nasib Mammadov <bduti09@gmail.com>
2020-08-02 20:21:58 +0400

    new-portfolio

---
+++ site3/assets/js/script.js
@@ -0,0 +1,13 @@
+// eto plavnaya prokrutka ili scroll $ oznacaet cto on na jquery
+// window oznacaet okno dalwe a ssilku i rel funkcuyu kak ona budet ranotat
+// mi rel='m_PageScroll2id' dobavili v header ssilku to
+//  cto xoteli sdelat scroll
+
+// $(window).on("load", function () {
+
+//   // Pagescroll to ID
+//   $("a[rel='m_PageScroll2id']").mPageScroll2id();
+
+//   // Video bacground vide.js
+//   $('#header').vide('/assets/video/girls');
+// });
