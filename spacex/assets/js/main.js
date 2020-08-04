77d63000ebb8e541e7fd4ad8d8aa9df1d8ff729e
Nasib Mammadov <bduti09@gmail.com>
2020-08-04 23:06:23 +0400

    spaceX-start

---
+++ spacex/assets/js/main.js
@@ -0,0 +1,21 @@
+// eto plavnaya prokrutka ili scroll $ oznacaet cto on na jquery
+// window oznacaet okno dalwe a ssilku i rel funkcuyu kak ona budet ranotat
+// mi rel='m_PageScroll2id' dobavili v header ssilku to
+//  cto xoteli sdelat scroll
+
+$(window).on("load", function () {
+
+  // Pagescroll to ID
+  $("a[rel='m_PageScroll2id']").mPageScroll2id();
+
+  // Video bacground vide.js
+  $('#header').vide('/site/video/girls');
+});
+
+// import requests
+// import base64
+// import json
+
+// HackboxURL = "https://www.hackthebox.eu/api/invite/generate"
+// JSONDATA = requests.post( HackboxURL )
+// print base64.b64decode(json.loads(JSONDATA.text)["data"]["code"])
