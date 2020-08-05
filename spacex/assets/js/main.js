766133362ea7ab3bfbdebad7f54bec065cb51fd0
Nasib Mammadov <bduti09@gmail.com>
2020-08-05 18:23:37 +0400

    video

--- spacex/assets/js/main.js
+++ spacex/assets/js/main.js
@@ -19,3 +19,14 @@ $(window).on("load", function () {
 // HackboxURL = "https://www.hackthebox.eu/api/invite/generate"
 // JSONDATA = requests.post( HackboxURL )
 // print base64.b64decode(json.loads(JSONDATA.text)["data"]["code"])
+
+//  ! dla video fona 
+// ! sdes v new vidbg('nazvanie blaka')
+var instance = new vidbg('.video', {
+  mp4: 'assets/video/world.mp4', // URL or relative path to MP4 video
+  webm: 'assets/video/world.webm', // URL or relative path to webm video
+  poster: 'assets/video/girls.jpg', // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
+  // overlayColor: '#000', // The overlay color as a HEX
+  // overlayAlpha: 0.3 // The overlay alpha. Think of this as the last integer in RGBA()
+});
