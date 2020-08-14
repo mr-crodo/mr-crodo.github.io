7e28fb2ff7fc66092f8d94eeaaa80ba6e02a0f9b
Nasib Mammadov <bduti09@gmail.com>
2020-08-14 21:12:00 +0400

    C:/Program Files/Git/assets/ teg add

--- rollsbaku/assets/js/cart.js
+++ rollsbaku/assets/js/cart.js
@@ -1,99 +1,114 @@
-// 1. Найти кнопку добавить корзину
+// Div внутри корзины, в который мы добавляем товары
+let cartWrapper = document.querySelector('.cart-wrapper');
+
+// 1. Найти кнопку "Добавить в корзину"
 let cartButtons = document.querySelectorAll('[data-cart]');
-console.log(cartButtons);
 
-// 2. Отследить кнопку добавить корзину
+// 2. Отседить клик на кнопке "Добавить в корзину"
 cartButtons.forEach(function (item) {
-  item.addEventListener('click', function () {
+	item.addEventListener('click', function () {
+		// 3. По клику Определяем какой товар добавляется в корзину
+		// (внутри какой карточки кликнули)
+		let card = this.closest('.card');
 
-    //? По клику определить по какой карте произошел клик
-    // 3. Определить какой товар добавляется в корзину(кнутри какой карточки кликнули)
-    let card = this.closest('.card');
+		// 3.1 Проверять если ли уже такой товар в корзине
+		// 3.1.2 Определяем ID добавляемого товара
+		let id = card.dataset.id;
+		let counterElement = card.querySelector('[data-counter]');
+		let counter = card.querySelector('[data-counter]').innerText;
+		let itemInCart = cartWrapper.querySelector(`[data-id="${id}"]`);
 
-    // 4. Собераем данные с этого товара(название, цена, количество порций)
+		if (itemInCart) {
+			// Есть в корзине
+			// 3.1.3 Если есть → тогда увеличиваем его кол-во
+			let counterElement = itemInCart.querySelector('[data-counter]');
+			counterElement.innerText =
+				parseInt(counterElement.innerText) + parseInt(counter);
+		} else {
+			// Нет в корзине
+			// 4. Собираем данные с этого товара
+			let imgSrc = card.querySelector('.product-img').getAttribute('src');
+			let title = card.querySelector('.item-title').innerText;
+			let itemsInBox = card.querySelector('[data-items-in-box]')
+				.innerText;
+			let weight = card.querySelector('.price__weight').innerText;
+			let price = card.querySelector('.price__currency').innerText;
 
-    // ***********
-    // let img = card.querySelector('.product-img');
-    // console.log(img);
+			// 5. Собранные данные подставим в шаблон для товара в корзине
+			let cartItemHTML = `
+							<div class="cart-item" data-id="${id}">
+								<div class="cart-item__top">
+									<div class="cart-item__img">
+										<img src="${imgSrc}" alt="">
+									</div>
+									<div class="cart-item__desc">
+										<div class="cart-item__title">${title}</div>
+										<div class="cart-item__weight">${itemsInBox} / ${weight}</div>
 
-    // let src = img.getAttribute('src');
-    // console.log(src);
-    // **************
-    // ! мы заменили то что в зеленной на то что ниже
-    let imgSrc = card.querySelector('.product-img').getAttribute('src');
+										<!-- cart-item__details -->
+										<div class="cart-item__details">
 
-    let title = card.querySelector('.item-title').innerText;
+											<div class="items items--small counter-wrapper">
+												<div class="items__control" data-action="minus">-</div>
+												<div class="items__current" data-counter>${counter}</div>
+												<div class="items__control" data-action="plus">+</div>
+											</div>
 
-    let itemsInBox = card.querySelector('[data-items-in-box]').innerText;
+											<div class="price">
+												<div class="price__currency">${price}</div>
+											</div>
 
-    let weight = card.querySelector('.price__weight').innerText;
+										</div>
+										<!-- // cart-item__details -->
 
-    let price = card.querySelector('.price__currency').innerText;
+									</div>
+								</div>
+							</div>`;
 
-    let counter = card.querySelector('[data-counter]').innerText;
+			// 6. Отобразим товар в корзине
+			cartWrapper.insertAdjacentHTML('beforeend', cartItemHTML);
+		}
 
+		toggleCartStatus();
 
-    console.log(imgSrc, title, itemsInBox, weight, price, counter);
-
-
-    // 5. Собранные данные подставим в шаблон для товара в корзине
-
-    let cartItemHTML = `<div class="cart-item">
-                        <div class="cart-item__top">
-                          <div class="cart-item__img">
-                            <img src="${imgSrc}" alt="">
-                          </div>
-                          <div class="cart-item__desc">
-                            <div class="cart-item__title">${title}</div>
-                            <div class="cart-item__weight">${itemsInBox} / ${weight}</div>
-
-                            <!-- cart-item__details -->
-                            <div class="cart-item__details">
-
-                              <div class="items items--small counter-wrapper">
-                                <div class="items__control" data-action="minus">-</div>
-                                <div class="items__current" data-counter>${counter}</div>
-                                <div class="items__control" data-action="plus">+</div>
-                              </div>
-
-                              <div class="price">
-                                <div class="price__currency">${price}</div>
-                              </div>
-
-                            </div>
-                            <!-- // cart-item__details -->
-
-                          </div>
-                        </div>
-                      </div>`;
-
-
-
-    console.log(cartItemHTML);
-
-
-    let cartWrapper = document.querySelector('.cart-wrapper');
-    // ! "insertAdjacentHTML" позволяет добавить нам нужную нам разметку в div
-    // ? Он принимает два аршумента(1. куда добавить'в начало блока, в конец блока, после-блока',
-    // ? 2.'что мы дабавляем')
-    cartWrapper.insertAdjacentHTML("beforeend", cartItemHTML);
-
-    // ! сдесь мы реализовали поиск атрибута по названию \data\ м обращаясь к классу 'classList'
-    // ! добавляем к имеющимуся классу c помощью 'add' новый класс не удаляя старый
-    document.querySelector('[data-cart-empty]').classList.add('d-none');
-
-  });
+		counterElement.innerText = 1;
+	});
 });
 
+function toggleCartStatus() {
 
+	// Показывасем или скрываем определенные элементы к корзине
+	// Проверяем есть ли в корзине товары (наличие тегов с классом .cart-item)
+	if (cartWrapper.querySelectorAll('.cart-item').length > 0) {
+		// Скрываем плашку "Корзина пуста"
+		document.querySelector('[data-cart-empty]').classList.add('none');
+		// Показываем стоимость заказа
+		document.querySelector('.cart-total').classList.remove('none');
+		// Показываем форму заказа
+		document.querySelector('#order-form').classList.remove('none');
+	} else {
+		// Показываем плашку "Корзина пуста"
+		document.querySelector('[data-cart-empty]').classList.remove('none');
+		// Скрываем стоимость заказа
+		document.querySelector('.cart-total').classList.add('none');
+		// Скрываем форму заказа
+		document.querySelector('#order-form').classList.add('none');
+	}
 
+	// Пересчитываем стоимость заказа
+	let totalPrice = 0;
 
+	cartWrapper.querySelectorAll('.cart-item').forEach(function(item){
+		let counter = item.querySelector('[data-counter]').innerText;
+		let priceOneItem = item.querySelector('.price__currency').innerText;
+		console.log(counter);
+		console.log(priceOneItem);
+		let price = parseInt(counter) * parseInt(priceOneItem);
+        console.log("toggleCartStatus -> price", price);
+		totalPrice = totalPrice + price;
+	})
 
+	console.log(totalPrice);
+	document.querySelector('.total-price').innerText = totalPrice;
 
-
-
-
-
-
-
-// 6. Отобразим товар в корзине
+}
--- rollsbaku/assets/js/counter.js
+++ rollsbaku/assets/js/counter.js
@@ -1,25 +1,27 @@
-// 1. Найдем книпки (элементы управления) (кнопки - и +)
-let buttons = document.querySelectorAll('[data-action]');
+let body = document.querySelector('body');
 
+body.addEventListener('click', function (event) {
+	// Проверяем чтобы клик был по кнопкам "Плюс" или "Минус"
+	if (event.target.hasAttribute('data-action')) {
+		// Работа со счетчиком
+		// Находим div c счетчиком
+		let counterWrapper = event.target.closest('.counter-wrapper');
+		let counter = counterWrapper.querySelector('[data-counter]');
 
-// 2. Для каждой кнопки вешаем событие клик
-buttons.forEach(function (item) {
-  item.addEventListener("click", function () {
-    // Находим div с счетчиком
-    let counterWrapper = this.closest('.counter-wrapper');
-    let counter = counterWrapper.querySelector('[data-counter]');
+		// 3. Для кнопки + → Увеличиваем
+		if (event.target.dataset.action === 'plus') {
+			counter.innerText = ++counter.innerText;
+		} else {
+			// 4. Для кнопки - → Проверяем на > 1 и Уменьшаем
+			if (parseInt(counter.innerText) > 1) {
+				// действие, если условие выполняется
+				counter.innerText = --counter.innerText; // Уменьшили счетчик на 1
+			} else if ( parseInt(counter.innerText) === 1 && counter.closest('.cart-wrapper') ) {
+				counter.closest('.cart-item').remove();
+			}
+		}
+		// Пересчитываем корзину
+		toggleCartStatus();
 
-    // 3. Для кнопки '+' -> Увеличиваем
-    if (this.dataset.action === "plus") {
-      counter.innerText++;
-    } else {
-      // 4. Для кнопки '-' -> Проверяем на > 1 и Уменшаем
-      if (parseInt(counter.innerText) > 1) {
-        // действие, если условие выполняется
-        counter.innerText--;
-      }
-
-    }
-
-  });
+	}
 });
