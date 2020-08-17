28436c0e5f8457fa75f2dfae66d4d684ed04db28
Nasib Mammadov <bduti09@gmail.com>
2020-08-17 22:29:04 +0400

    ancor

--- spacex/assets/js/mars.js
+++ spacex/assets/js/mars.js
@@ -1,7 +1,7 @@
 // var instance = new vidbg('.video', {
 //   mp4: 'assets/video/mars/mars.mp4', // URL or relative path to MP4 video
 //   webm: 'assets/video/mars/mars.webm', // URL or relative path to webm video
-//   poster: '/spacex/assets/video/world/poster.jpg', // URL or relative path to fallback image
+//   poster: '/spacex/spacex/assets/video/world/poster.jpg', // URL or relative path to fallback image
 //   overlay: false, // Boolean to display the overlay or not
 //   // overlayColor: '#000', // The overlay color as a HEX
 //   // overlayAlpha: 0.3 // The overlay alpha. Think of this as the last integer in RGBA()
@@ -13,5 +13,5 @@ $(window).on("load", function () {
   // $("a[rel='m_PageScroll2id']").mPageScroll2id();
 
   // Video bacground vide.js
-  $('#video-mars').vide('/spacex/assets/video/mars/mars');
+  $('#video-mars').vide('/spacex/spacex/assets/video/mars/mars');
 });
