017f433b14417bd5d418953d580c0992817da1cf
Nasib Mammadov <bduti09@gmail.com>
2020-08-20 18:50:15 +0400

    Changing the style pagination libraries Swiper slider JS

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -16,3 +16,36 @@ var wow = new WOW(
   }
 );
 wow.init();
+
+
+// var mySwiper = new Swiper('.swiper-container', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// })
+
+var mySwiper = new Swiper('.swiper-container', {
+  speed: 400,
+  spaceBetween: 100,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
