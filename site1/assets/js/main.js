90fa069131cc8dfeca415428181b372cd4587709
Nasib <bduti09@gmail.com>
2020-09-03 01:03:06 +0400

    Particle JS start

--- site1/assets/js/main.js
+++ site1/assets/js/main.js
@@ -1,3 +1,56 @@
+//! Particle JS
+(function () {
+  var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    w = canvas.width = innerWidth,
+    h = canvas.height = innerHeight,
+    particles = [],
+    properties = {
+      bgColor: 'rgba(17, 17, 19, 1)',
+      particleColor: 'rgba(255, 40, 40, 1)',
+      particleRadius: 3,
+      particleCount: 60
+
+    }
+
+  document.querySelector('.header').appendChild(canvas);
+
+
+  window.onresize = function () {
+    w = canvas.width = innerWidth,
+      h = canvas.height = innerHeight;
+  }
+
+  class Particle {
+    constructor() {
+      this.x = Math.random() * w;
+      this.y = Math.random() * h;
+    }
+
+    
+  }
+
+  function reDrawBackground() {
+    ctx.fillStyle = properties.bgColor;
+    ctx.fillRect(0, 0, w, h);
+  }
+
+  function loop() {
+
+    reDrawBackground();
+
+    requestAnimationFrame(loop);
+  }
+
+  function init() {
+    loop();
+  }
+
+  init();
+
+
+}());
+
 // eto plavnaya prokrutka ili scroll $ oznacaet cto on na jquery
 // window oznacaet okno dalwe a ssilku i rel funkcuyu kak ona budet ranotat
 // mi rel='m_PageScroll2id' dobavili v header ssilku to
