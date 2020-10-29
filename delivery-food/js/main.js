e2f2b5ef5732d36f7dd8a611e681d8d4936e1b2a
Nasib <bduti09@gmail.com>
2020-10-29 19:49:08 +0400

    day one delivery food day one

---
+++ delivery-food/js/main.js
@@ -0,0 +1,13 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+// ! Day one
+document.querySelector('.button-auth');
