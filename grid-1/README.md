1bc158d18399a4a95300d0fcf7ef559b63c14c42
Nasib <bduti09@gmail.com>
2021-03-05 17:10:57 +0400

    start grid project

---
+++ grid-1/README.md
@@ -0,0 +1,8 @@
+# npm install rimraf -g
+
+* И в папке `проекта` удалите папку node_modules с помощью:
+
+# rimraf node_modules
+
+* Если вы хотите рекурсивно удалить:
+* rimraf .\**\node_modules
--- grid-1/gulpfile.js
+++ grid-1/gulpfile.js
@@ -1,83 +1,260 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var rigger = require('gulp-rigger');
-var rimraf = require('rimraf');
-var fileinclude = require('gulp-file-include');
+// const fileinclude = require('gulp-file-include');
+
+let project_folder = 'build/';
+let source_folder = "src";
+
+let fs = require('fs');
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/",
+  },
+
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/style.scss",
+    js: source_folder + "/js/script.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    fonts: source_folder + "/fonts/**/*.ttf",
+  },
+
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+  },
+
+  clean: "./" + project_folder + "/"
+}
+
+let {
+  src,
+  dest
+} = require('gulp'),
+  gulp = require('gulp'),
+  browsersync = require("browser-sync").create(),
+  fileinclude = require("gulp-file-include"),
+  del = require("del"),
+  scss = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  group_media = require("gulp-group-css-media-queries"),
+  clean_css = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin"),
+  webp = require('gulp-webp'),
+  webphtml = require('gulp-webp-html'),
+  webpcss = require("gulp-webpcss"),
+  svgSprite = require('gulp-svg-sprite'), // gulp-svg-sprite
+  ttf2woff = require('gulp-ttf2woff'),
+  ttf2woff2 = require('gulp-ttf2woff2'),
+  fonter = require('gulp-fonter');
+
+const concat = require('gulp-concat');
+
+function browserSync(params) {
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/"
+    },
+    port: 3000,
+    notify: false
+  });
+}
 
 
-sass.compiler = require('node-sass');
+function html() {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(webphtml())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
 
 
+}
 
+// TODO: sdes sozdaem fayli dla SCSS
+function css() {
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(
+      group_media()
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 5 versions"],
+        cascade: true
+      })
+    )
 
-/* ------------ Styles compile ------------- */
-gulp.task('sass', function () {
-    return gulp.src('source/order-details/sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('build/css'))
-});
+    .pipe(webpcss({
+      webpClass: '.webp',
+      noWebpClass: '.no-webp'
+    }))
+    // ?viqrujaem  css fayl
+    .pipe(dest(path.build.css))
+    // ? sjimaem css fayl
+    .pipe(clean_css())
+    // ? pereimenovivaem dla toqo ctobi bilo dva fayla sjatiy i net
+    .pipe(
+      rename({
+        extname: ".min.css"
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
 
-/* ------------ Templates compile ------------- */
-gulp.task('riger', function () {
-    return gulp.src('source/order-details/*.html')
-        .pipe(rigger())
-        .pipe(fileinclude())
-        .pipe(gulp.dest('build'))
-        .pipe(browserSync.stream())
-});
+// TODO: sdes propisivaem funkciyu dla JS
+function js() {
+  return src([
+      // eto jquery esli est neobxodimist mojno podklycit
+      'node_modules/jquery/dist/jquery.min.js',
+      path.src.js
+    ])
 
-/* ------------ Copy fonts ------------- */
-gulp.task('copy:fonts', function () {
-    return gulp.src('source/order-details/fonts/**/*.*')
-        .pipe(gulp.dest('build/fonts'));
-});
+    
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(
+      uglify()
+    )
 
-/* ------------ Copy img ------------- */
-gulp.task('copy:img', function () {
-    return gulp.src('source/order-details/img/**/*.*')
-        .pipe(gulp.dest('build/img'));
-});
+    .pipe(
+      rename({
+        extname: ".min.js"
+      })
+    )
+    .pipe(concat('.min.js'))
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+    
+}
 
-/* ------------ Copy js ------------- */
-gulp.task('copy:js', function () {
-    return gulp.src('source/order-details/js/**/*.*')
-        .pipe(gulp.dest('build/js'));
-});
+// TODO: Dla img
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70
+      })
+    )
 
-/* ------------ Copy ------------- */
-gulp.task('copy', gulp.parallel('copy:fonts', 'copy:img', 'copy:js'));
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
 
-/* ------------ Delete ------------- */
-gulp.task('clean', function del(cb) {
-    return rimraf('build', cb);
-});
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{
+          removeViewBox: false
+        }],
+        interlaced: true,
+        optimizationLevel: 3 // 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
 
+}
 
+function fonts() {
+  src(path.src.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts));
 
+  return src(path.src.fonts)
+    .pipe(ttf2woff2())
+    .pipe(dest(path.build.fonts));
+}
 
-/* ------------ Watchers ------------- */
-gulp.task('watch', function () {
-    gulp.watch('source/order-details/**/*.html', gulp.series('riger'));
-    gulp.watch('source/order-details/sass/**/*.scss', gulp.series('sass'));
-    gulp.watch('source/order-details/js/**/*.js', gulp.series('copy:js'));
-    gulp.watch('source/order-details/img/**/*.js', gulp.series('copy:img'));
-});
+gulp.task('otf2ttf', function () {
+  return src([source_folder + '/fonts/**/*.otf'])
+    .pipe(fonter({
+      formats: ['ttf']
+    }))
+    .pipe(dest(source_folder + '/fonts/'));
+})
 
-/* -------- Server  -------- */
-gulp.task('server', function () {
-    browserSync.init({
-        server: {
-            port: 9000,
-            baseDir: "build"
+gulp.task('svgSprite', function () {
+  return gulp.src([source_folder + '/iconsprite/*.svg'])
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../icons/icons.svg", //sprite file name
+          // ?example mojno vklycit on mojet sozdavat html s primerami ikonok
+          example: true,
+          inline: true, // Prepare for inline embedding
+          symbol: true // Create a «symbol» sprite
+        },
+
+      },
+    }))
+    .pipe(dest(path.build.img))
+})
+
+// *****************************
+// TODO: sozdaem tut funkciyu ctobu podklycit ix k stilam
+
+function fontsStyle(params) {
+
+  let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+  if (file_content == '') {
+    fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+    return fs.readdir(path.build.fonts, function (err, items) {
+      if (items) {
+        let c_fontname;
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split('.');
+          fontname = fontname[0];
+          if (c_fontname != fontname) {
+            fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+          }
+          c_fontname = fontname;
         }
-    });
+      }
+    })
+  }
+}
 
-    gulp.watch('build/**/*').on('change', browserSync.reload);
-});
+function cb() {
 
-gulp.task('default', gulp.series(
-    'clean',
-    gulp.parallel('riger', 'sass', 'copy'),
-    gulp.parallel('watch', 'server')
-));
+}
+
+// *************************
+
+
+function watchFiles(params) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+}
+
+function clean(params) {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
+// TODO: eto scenariy vipolneniya funktiy
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
