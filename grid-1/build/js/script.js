586b8364afb95548df714792df85a76aba01e35f
Nasib <bduti09@gmail.com>
2021-03-31 01:30:54 +0400

    blog section finish

--- grid-1/build/js/script.js
+++ grid-1/build/js/script.js
@@ -1,18 +1,51 @@
-
 function testWebP(callback) {
 
   var webP = new Image();
   webP.onload = webP.onerror = function () {
-  callback(webP.height == 2);
+    callback(webP.height == 2);
   };
   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-  }
-  
-  testWebP(function (support) {
-  
+}
+
+testWebP(function (support) {
+
   if (support == true) {
-  document.querySelector('body').classList.add('webp');
-  }else{
-  document.querySelector('body').classList.add('no-webp');
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
   }
+});
+
+$(document).ready(function () {
+  $('works__btn button, .works__btn a').on('focus', function (e) {
+    $(this).parents('.works__element').addClass('works__element--active');
   });
+
+  $('works__btn button, .works__btn a').on('blur', function (e) {
+    $(this).parents('.works__element').removeClass('works__element--active');
+  });
+
+  function moveProgressBar(node, nodeLine, tooltip, animationLength = 1000) {
+    const progressElement = $(node);
+    progressElement.each(function (value, item) {
+      $(item).find(nodeLine).animate({
+        width: item.dataset.progressPercent + '%'
+      }, animationLength);
+      $(item).find(tooltip).show(animationLength);
+    });
+  }
+
+  let animate = true;
+
+  $(window).scroll(function () {
+
+    if ($('.skills').offset().top <= $(window).scrollTop()) {
+      if (animate) {
+        moveProgressBar('.progress__element', '.progress__line', '.progress__tooltip');
+      }
+      animate = false;
+    }
+  });
+
+
+});
