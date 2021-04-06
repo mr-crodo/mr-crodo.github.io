909a54d75cec5fd2a6e5c712d44369725d1d7490
Nasib <bduti09@gmail.com>
2021-04-06 20:08:21 +0400

    timer for site

--- food/src/js/main.js
+++ food/src/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // todo: Tabs
   const 
     tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent'),
@@ -6,7 +7,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   function hideTabContent() {
     tabsContent.forEach(item => {
-      item.style.display = 'none';
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
     });
 
     tabs.forEach(item => {
@@ -14,13 +16,83 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  function showTabContent(i) {
-    tabsContent[i].style.display = 'block';
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
     tabs[i].classList.add('tabheader__item_active');
   }
 
   hideTabContent();
-  showTabContent(0);
+  showTabContent();
 
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if(target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      })
+    }
+  });
+
+
+  // todo: TImer
+  const deadLine = '2021-04-30';
+
+  function getTimeRemaining(endtime) {
+    const
+      t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor(t / (1000 * 60 * 60) % 24), // sdes odni skobki otsutstvuyut
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+      };
+  }
+
+  function getZero(num) {
+
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+        return num;
+      }
+  }
+
+  function setClock(selector, endtime) {
+    const
+        timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); // dla toqo ctobi ne morqala stranica srazu zapuskaem funkciyu
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadLine);
 
 });
