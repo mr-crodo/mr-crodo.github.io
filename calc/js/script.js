515f68a934b5da4daf01e6e838904692864ef7ab
Nasib <bduti09@gmail.com>
2021-04-17 00:46:45 +0400

    js calc

---
+++ calc/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const
+  inputRub = document.querySelector('#rub'),
+  inputUsd = document.querySelector('#usd');
+
+  inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'calc/js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', ()=> {
+      if (request.readyState === 4 && request.status === 200) {
+        // console.log(request.response);
+        const data = JSON.parse(request.response);
+        inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+      } else {
+        inputUsd.value = "ALE qaqa Ishlemedi";
+      }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
+  });
