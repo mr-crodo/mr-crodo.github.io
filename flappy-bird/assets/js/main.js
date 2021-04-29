b935d5047b018542722911735cde88a344d1d5ab
Nasib <bduti09@gmail.com>
2021-04-29 16:15:09 +0400

    fixed

--- flappy-bird/assets/js/main.js
+++ flappy-bird/assets/js/main.js
@@ -14,7 +14,7 @@ var mainState = {
     game.stage.backgroundColor = '#71c5cf';
 
     game.load.image('bird', 'assets/kakashka/bird2.png');
-    game.load.image('pipe', 'assets/kakashka/pipew.png');
+    game.load.image('pipe', 'assets/kakashka/bird1.png');
 
     // Load the jump sound
     game.load.audio('jump', 'assets/sound/miko.wav');
