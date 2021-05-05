7cf7e2b5005cdc9c03f7ff00f6229369c0436bc9
Nasib <bduti09@gmail.com>
2021-05-06 01:17:14 +0400

    add flappy bird upgrade

---
+++ flappybird/flappyBird.js
@@ -0,0 +1,102 @@
+let cvs = document.querySelector('#canvas');
+let ctx = cvs.getContext('2d');
+
+//Load image
+
+let
+  bird = new Image(),
+  bg = new Image(),
+  fg = new Image(),
+  pipeNorth = new Image(),
+  pipeSouth = new Image();
+
+bird.src = "images/bird.png";
+bg.src = "images/bg-1.png";
+fg.src = "images/fbs-04.png";
+pipeNorth.src = "images/pipe3.png";
+pipeSouth.src = "images/pipe4.png";
+
+
+// some letiables
+
+let gap = 85;
+let constant;
+
+
+let bX = 10;
+let bY = 150;
+
+let gravity = 1.5;
+
+let score = 0;
+
+// audi files
+
+let fly = new Audio();
+let scor = new Audio();
+
+fly.src = "sounds/fly.mp3";
+scor.src = "sounds/score.mp3";
+
+//on key down
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+  bY -= 25;
+  fly.play();
+}
+
+// pipe coordinates
+
+let pipe = [];
+
+pipe[0] = {
+  x : cvs.width,
+  y : 0
+};
+
+// draw images 
+
+function draw() {
+  ctx.drawImage(bg, 0, 0);
+
+  for(let i = 0; i < pipe.length; i++) {
+    constant = pipeNorth.height + gap;
+    ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+    ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+
+    pipe[i].x--;
+
+    if(pipe[i].x == 125) {
+      pipe.push({
+        x : cvs.clientWidth,
+        y : Math.floor(Math.random()*pipeNorth.height) - pipeNorth.height
+      });
+    }
+
+    // detect collision
+
+    if(bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) || bY + bird.height >= cvs.height - fg.height) {
+      location.reload(); // reload the page
+    }
+
+    if(pipe[i].x == 5) {
+      score++;
+      scor.play();
+    }
+  }
+
+  ctx.drawImage(fg, 0, cvs.height - fg.height);
+
+  ctx.drawImage(bird,bX,bY);
+
+  bY += gravity;
+
+  ctx.fillStyle = "#000";
+  ctx.font = "20px Verdana";
+  ctx.fillText("Score : "+score,10,cvs.height-20);
+
+  requestAnimationFrame(draw);
+}
+
+draw();
