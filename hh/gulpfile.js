1fb7ef60b2b3011be72eb641ed8fa78c3c38f6b8
Nasib Mammadov <bduti09@gmail.com>
2021-05-25 17:04:52 +0400

    start new project hh day one

---
+++ hh/gulpfile.js
@@ -0,0 +1,278 @@
+let project_folder = 'build/';
+let source_folder = "#src";
+
+let fs = require('fs');
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/",
+    video: project_folder + "/video/",
+  },
+
+  src: {
+    html: [source_folder + "/**/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/*.scss",
+    js: source_folder + "/js/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    fonts: source_folder + "/fonts/**/*.ttf",
+    video: source_folder + "/video/**/*.{mp4,ogv,webm,avi}",
+  },
+
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    video: source_folder + "/video/**/*.{mp4,ogv,webm,avi}",
+  },
+
+  clean: "./" + project_folder + "/"
+}
+
+let {
+  src,
+  dest
+} = require('gulp'),
+  gulp = require('gulp'),
+  browsersync = require("browser-sync").create(),
+  fileinclude = require("gulp-file-include"),
+  del = require("del"),
+  scss = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  group_media = require("gulp-group-css-media-queries"),
+  clean_css = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin"),
+  webp = require('gulp-webp'),
+  webphtml = require('gulp-webp-html'),
+  webpcss = require("gulp-webpcss"),
+  svgSprite = require('gulp-svg-sprite'), // gulp-svg-sprite
+  ttf2woff = require('gulp-ttf2woff'),
+  ttf2woff2 = require('gulp-ttf2woff2'),
+  // surge = require('gulp-surge'),
+  fonter = require('gulp-fonter');
+
+const concat = require('gulp-concat');
+
+function browserSync(params) {
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/"
+    },
+    port: 3000,
+    notify: false
+  });
+}
+
+
+function html() {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(webphtml())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
+}
+
+function video() {
+  return src(path.src.video)
+    // .pipe(fileinclude())
+    // .pipe(webphtml())
+    .pipe(dest(path.build.video))
+    .pipe(browsersync.stream())
+}
+
+// TODO: sdes sozdaem fayli dla SCSS
+function css() {
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(
+      group_media()
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 5 versions"],
+        cascade: true
+      })
+    )
+
+    .pipe(webpcss({
+      webpClass: '.webp',
+      noWebpClass: '.no-webp'
+    }))
+    // ?viqrujaem  css fayl
+    .pipe(dest(path.build.css))
+    // ? sjimaem css fayl
+    .pipe(clean_css())
+    // ? pereimenovivaem dla toqo ctobi bilo dva fayla sjatiy i net
+    .pipe(
+      rename({
+        extname: ".min.css"
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+}
+
+// TODO: sdes propisivaem funkciyu dla JS
+function js() {
+  return src([
+      // eto jquery esli est neobxodimist mojno podklycit
+      'node_modules/jquery/dist/jquery.min.js',
+      path.src.js
+    ])
+
+
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(
+      uglify()
+    )
+
+    .pipe(
+      rename({
+        extname: ".min.js"
+      })
+    )
+    .pipe(concat('.min.js'))
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+
+}
+
+// TODO: Dla img
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70
+      })
+    )
+
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{
+          removeViewBox: false
+        }],
+        interlaced: true,
+        optimizationLevel: 3 // 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+
+}
+
+function fonts() {
+  src(path.src.fonts)
+    .pipe(ttf2woff())
+    .pipe(dest(path.build.fonts));
+
+  return src(path.src.fonts)
+    .pipe(ttf2woff2())
+    .pipe(dest(path.build.fonts));
+}
+
+gulp.task('otf2ttf', function () {
+  return src([source_folder + '/fonts/**/*.otf'])
+    .pipe(fonter({
+      formats: ['ttf']
+    }))
+    .pipe(dest(source_folder + '/fonts/'));
+})
+
+gulp.task('svgSprite', function () {
+  return gulp.src([source_folder + '/img/svg/*.svg'])
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: "../icons/icons.svg", //sprite file name
+          // ?example mojno vklycit on mojet sozdavat html s primerami ikonok
+          example: true,
+          inline: true, // Prepare for inline embedding
+          symbol: true // Create a «symbol» sprite
+        },
+
+      },
+    }))
+    .pipe(dest(path.build.img))
+})
+
+// *****************************
+// gulp.task('publicWeb', [], function () {
+//   return surge({
+//     project: './build', // Path to your static build directory
+//     domain: 'crodo-grid.surge.sh' // Your domain or Surge subdomain
+//   });
+// });
+// TODO: sozdaem tut funkciyu ctobu podklycit ix k stilam
+
+function fontsStyle(params) {
+
+  let file_content = fs.readFileSync(source_folder + '/scss/_fonts.scss');
+  if (file_content == '') {
+    fs.writeFile(source_folder + '/scss/_fonts.scss', '', cb);
+    return fs.readdir(path.build.fonts, function (err, items) {
+      if (items) {
+        let c_fontname;
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split('.');
+          fontname = fontname[0];
+          if (c_fontname != fontname) {
+            fs.appendFile(source_folder + '/scss/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+          }
+          c_fontname = fontname;
+        }
+      }
+    })
+  }
+}
+
+function cb() {
+
+}
+
+// *************************
+
+
+function watchFiles(params) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.video], video);
+}
+
+function clean(params) {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts, video), fontsStyle);
+// TODO: eto scenariy vipolneniya funktiy
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+
+
+
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.fonts = fonts;
+exports.video = video;
+exports.images = images;
+exports.fontsStyle = fontsStyle;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
