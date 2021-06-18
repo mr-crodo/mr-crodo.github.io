0357287ac6ee0e7c5ba8b8f50bed739a326f48cd
Nasib Mammadov <bduti09@gmail.com>
2021-06-18 14:53:10 +0400

    fixed

--- board/app.js
+++ board/app.js
@@ -1,23 +1,25 @@
 const board = document.querySelector('#board')
-const SQUARES_NUMBER = 800
+const SQUARES_NUMBER = 10000
 const colors = ['red', 'blue', 'green', 'yellow']
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
 
-  square.addEventListener('mouseover', () => setColor(square))
-  square.addEventListener('mouseleave', () => removeColor(square))
+  square.addEventListener('mouseover', setColor)
+  square.addEventListener('mouseleave', removeColor)
   board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target
   const color = getRandomColor()
   element.style.backgroundColor = color
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
+function removeColor(event) {
+  const element = event.target
   element.style.backgroundColor = '#1d1d1d'
   element.style.boxShadow = `0 0 2px #000`
 }
