02bf44c5e9f8e61aa68df2361de4f522d422a620
Nasib Mammadov <bduti09@gmail.com>
2021-07-08 19:20:30 +0400

    day 2 fixed

--- lamoda/js/app.js
+++ lamoda/js/app.js
@@ -2,6 +2,9 @@ const
   headerCityButton = document.querySelector('.header__city-button'),
   subheaderCart = document.querySelector('.subheader__cart'),
   cartOverlay = document.querySelector('.cart-overlay');
+// obrazaem hash s substring
+  let hash = location.hash.substring(1);
+  
 
 
 // ternarniy operator
@@ -28,7 +31,7 @@ const disableScroll = () => {
     padding-right: ${widthScroll}px;
   `;
 };
-const enableScroll = () =>{
+const enableScroll = () => {
   document.body.style.cssText = '';
   window.scroll({
     top: document.body.dbScrollY
@@ -47,21 +50,138 @@ const cartModalClose = () => {
   cartOverlay.classList.remove('cart-overlay-open')
   enableScroll()
 };
-subheaderCart.addEventListener('click', cartModalOpen);
 
 // zakritie po Esk
 document.addEventListener('keydown', function (event) {
-      if (event.code == 'Escape') {
-        cartModalClose();
+  if (event.code == 'Escape') {
+    cartModalClose();
+  }
+})
+
+
+//! zapros s DB universalnaya funkciy dla zaprosa lyboqo servera
+//?zapros s DB universalnaya funkciy dla zaprosa lyboqo servera
+const getData = async () => {
+  const data = await fetch('db.json');
+
+  if (data.ok) {
+    return data.json()
+  } else {
+    throw new Error(`Данные небыли получены, ошибка ${data.status} ${data.statusText}`)
+  }
+}
+//? zapros s DB universalnaya funkciy dla zaprosa lyboqo servera
+// getData()
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+
+// konkretno dla naweqo prilojeniya
+// peredaem vtorim parametrom hash v vide value
+const getGoods = (callback, value) => {
+  getData()
+    .then(data => {
+      if(value) {
+        callback(data.filter(item => item.category === value))
+      } else {
+        callback(data)
       }
     })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+// getGoods((data) => {
+//   console.warn(data);
+// })
+
+// vizovi funksii doljni naxoditsa pod funkciyami
+subheaderCart.addEventListener('click', cartModalOpen);
 
 // delegirovanie
 cartOverlay.addEventListener('click', event => {
   const target = event.target
-  
   // ! krome -- target."classList.contains" mojno ispolzovat "matches" no s selektorom tocki ili rewetki
-  if(target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+  if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
     cartModalClose();
   }
 })
+
+
+try {
+  console.log(hash)
+  const goodsList = document.querySelector('.goods__list')
+  if (!goodsList) {
+    throw "This is not a good page"
+  }
+  
+  const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+    // es2015 destrukturizaciya
+    //? const { id, preview, cost, brand, name, sizes } = data;
+    // vmesto etoqo verxneye
+    // const id = data.id;
+    // const preview = data.preview;
+    // const cost = data.cost;
+    // const brand = data.brand;
+    // const name = data.name;
+    // const sizes = data.sizes;
+    
+    const li = document.createElement('li');
+
+    li.classList.add('goods__item');
+
+    li.innerHTML = `
+        <article class="good">
+            <a class="good__link-img" href="card-good.html#${id}">
+                <img class="good__img" src="goods-image/${preview}" alt="">
+            </a>
+            <div class="good__description">
+                <p class="good__price">${cost} &#8381;</p>
+                <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                ${sizes ? 
+                  `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(" ")}</span></p>` :
+                  ''}
+                <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+            </div>
+        </article>
+    `;
+
+    return li;
+  };
+
+  const renderGoodsList = data => {
+    goodsList.textContent = '';
+    
+    // for (let i =0; i < data.length; i++) {
+    //   console.log('for',data[i]);
+    // }
+
+    // for(const item of data) {
+    //   console.log('for of', item);
+    // }
+
+    data.forEach((item) => {
+      const card = createCard(item);
+      goodsList.append(card)
+    })
+  };
+
+  window.addEventListener('hashchange', () => {
+    const goodsTitle = document.querySelector('.goods__title');
+    hash = location.hash.substring(1)
+    if()
+      goodsTitle.innerHTML = `
+      <span class="goods__title">${hash}</span>
+      `
+    getGoods(renderGoodsList, hash)
+  })
+// !peredaem hash
+  getGoods(renderGoodsList, hash);
+} catch (err) {
+  console.warn(err);
+}
